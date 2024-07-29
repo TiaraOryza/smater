95271417ef603620bc1fbb789f46e4645ba108a7
Shindu Astawa <shinduastawa@gmail.com>
2024-07-29 12:24:57 +0800

    import fitur

---
+++ app/Imports/KriteriaImport.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace App\Imports;
+
+use App\Models\KriteriaModel;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+
+class KriteriaImport implements ToModel, WithHeadingRow
+{
+    public function model(array $row)
+    {
+        // Pastikan bahwa keterangan tidak null
+        if (!isset($row['keterangan']) || !isset($row['kode_kriteria']) || !isset($row['prioritas']) || !isset($row['bobot'])) {
+            return null;
+        }
+
+        return new KriteriaModel([
+            'keterangan' => $row['keterangan'],
+            'kode_kriteria' => $row['kode_kriteria'],
+            'prioritas' => $row['prioritas'],
+            'bobot' => $row['bobot'],
+        ]);
+    }
+}
+
